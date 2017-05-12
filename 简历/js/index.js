6cd8e1d72d801c66a611d8f487c2d4d5f6ad4e17
iProgramme <747137830@qq.com>
2017-05-12 23:34:02 +0800

    简历和获取宽高

--- 简历/js/index.js
+++ 简历/js/index.js
@@ -5,18 +5,19 @@ $(function() {
     var index = 0;
     var box = document.querySelector('.index_main');
     var time = null;
-    var height = document.documentElement.clientHeight;
+    var height = document.documentElement.clientHeight-70;
     var section = document.querySelectorAll('.section');
     var colors = [
-        'linear-gradient(to bottom,rgba(244,10,10,.9),rgba(227,97,137,.9))',
-        'linear-gradient(to bottom,rgba(227,97,137,.9),rgba(64,200,243,.9))',
-        'linear-gradient(to bottom,rgba(64,200,243,.9),rgba(60,167,241,.9))',
-        'linear-gradient(to bottom,rgba(60,167,241,.9),rgba(212,81,190,.9))',
+        'rgba(0,0,0,.7)',
+        'rgba(0,0,0,.8)',
+        'rgba(0,0,0,.7)',
+        'rgba(0,0,0,.7)',
     ]
     // 先将几个大的div旋转一下
     section[0].style.transform = "translateZ("+height/2+"px)"
     section[1].style.transform = "translateY("+height/2+"px) rotateX(-90deg)"
     section[2].style.transform = "translateZ("+(-height)/2+"px) rotateY(180deg) rotateZ(180deg)";
+    section[3].style.transform = "translateY("+(-height)/2+"px) rotateX(90deg)";
     // 把高度给大盒子和每个小盒子,防止3d旋转的时候出问题
     box.style.height = height+"px";
     for (var i = 0; i < section.length; i++) {
@@ -27,7 +28,6 @@ $(function() {
     $('#accordion').collapse()
     // 每次滚屏结束之后调用函数
     box.addEventListener('transitionend',function () {
-        console.log(index%4)
         animationY(index)
     })
 
@@ -52,22 +52,24 @@ $(function() {
         $('.index_up').addClass('zoomOutUp');
         setTimeout(function () {
             $("nav.navbar").addClass('bounceInUp').removeClass("collapse")
-        },1500)
+        },1000)
         setTimeout(function () {
-            $(".index_main").removeClass('collapse').addClass("rollIn")
-        },3000)
+            $(".index_main").removeClass('collapse').addClass("rollIn");
+            box.style.transform = "translateZ(-300px)";
+        },2000)
         // 滚屏事件
         window.onmousewheel = function (e) {
             clearTimeout(time)
             box.classList.remove('rollIn')
             time = setTimeout(function () {
                 e.wheelDelta > 0 ? index++ : index--;
-                console.log(index)
+                // console.log(index)
                 box.style.transform = "translateZ(-300px) rotateX("+index*(-90)+"deg)";
                 box.style.transition = "transform 1s";
             },100)
         }
     })
+
 });
 
 
@@ -76,9 +78,24 @@ $(function() {
  * @param index 当前屏的索引
  */
 function animationY(index) {
-    index = index%4
-    if (index == 0) {
+    console.log(index)
 
+    index = Math.abs(index%4)
+    if (index == 1) {
+        var arr = ['40%','50%','70%','50%','70%','90%'];
+        console.log($(".progress-bar"))
+
+        $(".progress-bar").each(function (index,item) {
+            console.log(index)
+            $(item).css({
+                'transition':'width 1s',
+                'width':arr[index]
+            })
+        })
     }
 
 }
+
+
+
+
